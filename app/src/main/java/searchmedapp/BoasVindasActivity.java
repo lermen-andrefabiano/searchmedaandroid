ca79f4a29a1e08c6d83964d5679fb0db5afcf18f
Andre Fabiano Lermen <lermen.andrefabiano@gmail.com>
2016-06-26 00:31:36 -0300

    Favorito

--- app/src/main/java/searchmedapp/BoasVindasActivity.java
+++ app/src/main/java/searchmedapp/BoasVindasActivity.java
@@ -35,9 +35,6 @@ public class BoasVindasActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
